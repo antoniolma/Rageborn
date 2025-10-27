9e60e61cbd3e42ed77d01adc11b8da6937d0e000
antoniolucasma <antoniolucasmda@gmail.com>
2025-10-27 16:54:43 -0300

    Animacao Player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private SpriteRenderer spriteRenderer; 
 
     public float speed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -14,10 +16,22 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
 
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
         rb.MovePosition(rb.position + speed * movement.normalized * Time.fixedDeltaTime);
+
+        // Verifica se esta correndo
+        if (moveVertical != 0 || moveHorizontal != 0)
+        {
+            _animator.SetBool("isRunning", true);
+            _animator.SetFloat("LastInputX", moveHorizontal);
+            _animator.SetFloat("LastInputY", moveVertical);
+        }
+        else _animator.SetBool("isRunning", false);
+
+        _animator.SetFloat("InputX", moveHorizontal);
+        _animator.SetFloat("InputY", moveVertical);
     }
 }
